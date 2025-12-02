52302c79b69161a06902c5ca2979a418b5c893f5
Tomas <Tomas70333481@gmail.com>
2025-12-02 10:09:45 +0200

    Add files via upload

---
+++ day2/part1/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	intervals := readFile(convert)[0]
+
+	sum := 0
+	for _, interval := range intervals {
+		for i := interval[0]; i <= interval[1]; i++ {
+			if isInvalid(i) {
+				sum += i
+			}
+		}
+	}
+
+	fmt.Println(sum)
+}
+
+func isInvalid(n int) bool {
+	str := strconv.Itoa(n)
+	return len(str)%2 == 0 && str[0:len(str)/2] == str[len(str)/2:]
+}
+
+func readFile[T any](convert func(string) T) []T {
+	f, err := os.Open("day2/part1/input.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	lines := make([]T, 0)
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		lines = append(lines, convert(scanner.Text()))
+	}
+	if err = scanner.Err(); err != nil {
+		panic(err)
+	}
+
+	return lines
+}
+
+func convert(line string) [][2]int {
+	intervals := make([][2]int, 0)
+
+	parts := strings.Split(line, ",")
+	for _, part := range parts {
+		numbers := strings.Split(part, "-")
+		n1, err := strconv.Atoi(numbers[0])
+		if err != nil {
+			panic(err)
+		}
+		n2, err := strconv.Atoi(numbers[1])
+		if err != nil {
+			panic(err)
+		}
+
+		intervals = append(intervals, [2]int{n1, n2})
+	}
+
+	return intervals
+}
